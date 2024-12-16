ec726c5b3c8ee72fa08caad48ea3310ad63e2996
qcloud <ubuntu@localhost.localdomain>
2024-12-16 10:56:14 +0800

    完善前端1.0

---
+++ pythonwalrus.py
@@ -0,0 +1,108 @@
+# Example of uploading and downloading a file to / from the Walrus service
+# Using the walrus client json input & output facilities.
+
+# Std lib imports
+import os
+import subprocess #创建进程，可以用来运行命令行
+import json
+import tempfile
+import base64
+
+import requests
+
+from utils import num_to_blob_id, PATH_TO_WALRUS, PATH_TO_WALRUS_CONFIG, FULL_NODE_URL
+
+try:
+    # Create a 1MiB file of random data
+    # random_data = os.urandom(1024 * 1024)
+    # tmp = tempfile.NamedTemporaryFile(delete=False)
+    # tmp.write(random_data)
+    # tmp.close()
+    file_path = "text.txt"
+
+    # Part 1. Upload the file to the Walrus service
+    store_json_command = f"""{{ "config" : "{PATH_TO_WALRUS_CONFIG}",
+        "command" : {{ "store" :
+        {{ "file" : "{file_path}", "epochs" : 2  }}}}
+    }}"""
+    result = subprocess.run(
+        [PATH_TO_WALRUS, "json"],
+        text=True,
+        capture_output=True,
+        input=store_json_command,    #这一大块相当于执行了walrus json命令，也就是直接在命令行中执行，其返回值就是命令行执行命令之后显示的一大串结果，result就是显示的结果
+    )
+
+    assert result.returncode == 0
+
+    # Parse the response and display key information
+    json_result_dict = json.loads(result.stdout.strip())
+    if "newlyCreated" in json_result_dict:
+        blob_id = json_result_dict["newlyCreated"]["blobObject"]["blobId"]
+        sui_object_id = json_result_dict["newlyCreated"]["blobObject"]["id"]
+    elif "alreadyCertified" in json_result_dict:
+        blob_id = json_result_dict["alreadyCertified"]["blobObject"]["blobId"]
+        sui_object_id = json_result_dict["alreadyCertified"]["blobObject"]["id"]
+    else:
+        raise ValueError("Unexpected response from Walrus")
+
+    print(
+        f"Upload Blob ID: {blob_id}"
+    )
+
+    print(f"Certificate in Object ID: {sui_object_id}")
+
+    # # Part 2. Download the file from the Walrus service
+    read_json_command = f"""{{ "config" : "{PATH_TO_WALRUS_CONFIG}",
+        "command" : {{ "read" :
+        {{ "blobId" : "{blob_id}" }}}}
+    }}"""
+    result = subprocess.run(
+        [PATH_TO_WALRUS, "json"],
+        text=True,
+        capture_output=True,
+        input=read_json_command,
+    )
+
+    assert result.returncode == 0
+
+    # # Parse the response and display key information
+    json_result_dict = json.loads(result.stdout.strip())
+    downloaded_data = base64.b64decode(json_result_dict["blob"])
+    # assert downloaded_data == random_data
+
+    print(
+        f"Download Blob ID: {json_result_dict['blobId']} Size {len(downloaded_data)} bytes"
+    )
+
+    # # Part 3. Check the availability of the blob
+    request = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "sui_getObject",
+        "params": [
+            sui_object_id,
+            {
+                "showType": True,
+                "showOwner": False,
+                "showPreviousTransaction": True,
+                "showDisplay": False,
+                "showContent": True,
+                "showBcs": False,
+                "showStorageRebate": False,
+            },
+        ],
+    }
+    response = requests.post(FULL_NODE_URL, json=request)
+    object_content = response.json()["result"]["data"]["content"]
+    print("Object content:")
+    print(json.dumps(object_content, indent=4))
+
+    # # Check that the blob ID matches the one we uploaded
+    # blob_id_downloaded = int(object_content["fields"]["blob_id"])
+    # if num_to_blob_id(blob_id_downloaded) == blob_id:
+    #     print("Blob ID matches certificate!")
+    # else:
+    #     print("Blob ID does not match")
+
+finally:
+    print("end")
